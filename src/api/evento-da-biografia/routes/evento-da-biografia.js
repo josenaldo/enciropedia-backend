2aa9a3a66a553686649c1d61e6ed724fe9d8722a
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-03-17 15:31:47 -0300

    Criação de novos conterúdos

---
+++ src/api/evento-da-biografia/routes/evento-da-biografia.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * evento-da-biografia router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::evento-da-biografia.evento-da-biografia');
---
+++ src/api/evento-da-biografia/services/evento-da-biografia.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * evento-da-biografia service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::evento-da-biografia.evento-da-biografia');
