549fd2a4783712aa9227f673c60bca935eb691b7
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-03-17 23:53:33 -0300

    Instalação do plugin de sinconização

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', '7a952a58e845d75b6674e759595eb3be'),
-  },
+    auth: {
+        secret: env("ADMIN_JWT_SECRET", "7a952a58e845d75b6674e759595eb3be"),
+    },
+    watchIgnoreFiles: ["**/config/sync/**"],
 });
